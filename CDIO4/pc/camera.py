0cddd8f9b99f5ecbe5b7ec7e19f998a8085ba937
goli <golbashaidari@gmail.com>
2023-06-09 18:51:55 +0200

    I added method for all onbject detection such as obstacle, gate, goldball, ball and wall.

--- CDIO4/pc/camera.py
+++ CDIO4/pc/camera.py
@@ -50,7 +50,72 @@ def detectRobot(frame):
         break
     return robotPosition
 
-
-#need implementation!!! 
+ 
 def detect_wall(frame): 
-    return (0,0)
+    rf = Roboflow(api_key="cMW2MDLBCvueMKT3Gbfj")
+    project = rf.workspace().project("robotdetection-jvv38")
+    model = project.version(1).model
+
+    wallPosition= (0,0)
+    while True:
+        # Use the model to detect the wall
+        detections = model.predict(np.asarray(frame), confidence=20, overlap= 5)
+
+        # Get the position of the center of each wall
+        for detection in detections:
+            if detection["class"] == "wall":
+                x = int(detection["x"])
+                y = int(detection["y"])
+                print("Wall position: ({}, {})".format(x, y))
+                # Draw a circle around the detected wall
+                cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
+                wallPosition= (x,y)
+        break
+    return wallPosition
+    
+
+
+def detectGate(frame): 
+    rf = Roboflow(api_key="cMW2MDLBCvueMKT3Gbfj")
+    project = rf.workspace().project("robotdetection-jvv38")
+    model = project.version(1).model
+
+    gatePosition= (0,0)
+    while True:
+        # Use the model to detect the gate
+        detections = model.predict(np.asarray(frame), confidence=20, overlap= 5)
+
+        # Get the position of the center of each gate
+        for detection in detections:
+            if detection["class"] == "gate":
+                x = int(detection["x"])
+                y = int(detection["y"])
+                print("Gate position: ({}, {})".format(x, y))
+                # Draw a circle around the detected gate
+                cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
+                gatePosition= (x,y)
+        break
+    return gatePosition
+    
+
+def detectObstacle(frame): 
+    rf = Roboflow(api_key="cMW2MDLBCvueMKT3Gbfj")
+    project = rf.workspace().project("robotdetection-jvv38")
+    model = project.version(1).model
+
+    obstaclePosition= (0,0)
+    while True:
+        # Use the model to detect the obstacle
+        detections = model.predict(np.asarray(frame), confidence=20, overlap= 5)
+
+        # Get the position of the center of each obstacle
+        for detection in detections:
+            if detection["class"] == "obstacle":
+                x = int(detection["x"])
+                y = int(detection["y"])
+                print("Obstacle position: ({}, {})".format(x, y))
+                # Draw a circle around the detected obstacle
+                cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
+                obstaclePosition= (x,y)
+        break
+    return obstaclePosition
