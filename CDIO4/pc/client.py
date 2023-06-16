313cf82c1e7402be3c0e9df7cd6c19149a118ccf
Jawsen <s205414@dtu.dk>
2023-06-16 14:10:00 +0200

    Logic to move robot

--- CDIO4/pc/client.py
+++ CDIO4/pc/client.py
@@ -5,6 +5,7 @@ import math
 
 IP="172.20.10.3"
 PORT=6666
+robotHeading = 'SOUTH'
 
 
 def run():   
@@ -50,17 +51,172 @@ def calculateDistance(position1, position2):
     print("distance :!!!!")
     print((nx, ny))
     return (nx,ny)
+
+def moveRobot():
+    distance = calculateDistance()
+    nx = distance.nx
+    ny = distance.ny
+
+    def southCase():
+        if nx > 0 and ny > 0:
+            moveForward(ny)
+            turnLeft()
+            moveForward(nx)
+            robotHeading = 'EAST'
+            pass
+            
+
+        elif nx < 0 and ny > 0:
+            moveForward(ny)
+            turnRight()
+            moveForward(nx)
+            robotHeading = 'WEST'
+            pass
+            
+        
+        elif nx > 0 and ny < 0:
+            moveBackward(ny)
+            turnLeft()
+            moveForward(nx)
+            robotHeading = 'EAST'
+            pass
+            
+
+        elif nx < 0 and ny < 0:
+            moveBackward(ny)
+            turnRight()
+            moveForward(nx)
+            robotHeading = 'WEST'
+            pass
+
+        return robotHeading
+        
+        
+    def northCase():
+        if nx >0 and ny >0:
+            moveBackward(ny)
+            turnRight()
+            moveForward(nx)
+            robotHeading = 'EAST'
+            pass
+
+        elif nx < 0 and ny >0:
+            moveBackward(ny)
+            turnLeft()
+            moveForward(nx)
+            robotHeading ='WEST'
+            pass
+
+        elif nx > 0 and ny <0:
+            moveForward(ny)
+            turnRight()
+            moveForward(nx)
+            robotHeading = 'EAST'
+            pass
+        
+        elif nx < 0 and ny < 0:
+            moveForward(ny)
+            turnLeft()
+            moveForward(nx)
+            robotHeading = 'WEST'
+            pass
+
+        return robotHeading
+
+    
+
+
+    def eastCase():
+
+        if nx >0 and ny >0:
+            moveForward(nx)
+            turnRight()
+            moveForward(ny)    
+            robotHeading = 'SOUTH'
+
+        elif nx < 0 and ny >0:
+            moveBackward(nx)
+            turnRight
+            moveForward(ny)
+            robotHeading = 'SOUTH'
+        
+        elif nx > 0 and ny <0:
+            moveForward(nx)
+            turnLeft
+            moveForward(ny)
+            robotHeading = 'NORTH'
+
+        elif nx < 0 and ny < 0:
+            moveBackward(nx)
+            turnLeft
+            moveForward(ny)
+            robotHeading = 'NORTH'
+
+
+        return robotHeading
+
+
+    def westCase():
+
+
+        if nx >0 and ny >0:
+            moveBackward(nx)
+            turnLeft
+            moveForward(ny)
+            robotHeading = 'South'
+            
+
+        elif nx < 0 and ny >0:
+            moveForward(nx)
+            turnLeft
+            moveForward(ny)
+            robotHeading = 'South'
+        
+        elif nx > 0 and ny <0:
+            moveBackward(nx)
+            turnRight
+            moveForward(ny)
+            robotHeading = 'NORTH'
+
+            
+        
+
+        elif nx < 0 and ny < 0:
+            moveForward(nx)
+            turnRight
+            moveForward(ny)
+            robotHeading ='NORTH'
+
+        return robotHeading
+        
+    switch = {
+        'North': northCase,
+        'South': southCase,
+        'East': eastCase,
+        'West': westCase,
+    }
+
+    # Call the function retrieved from dictionary
+    func = switch.get(robotHeading)
+    if func:
+        func()
+    else:
+        print("Invalid heading")
+
+
 #Choses a random ball
 def chooseRandomBall(ballsPositions):
     randomIndex = random.randint(0, len(ballsPositions) - 1)
     return ballsPositions[randomIndex]
 
 
-def sendToRobot(randomBall,distance):
+def sendToRobot(randomBall,distance,instruction):
     server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
     server.connect((IP, PORT))
 
-    
+    server.send(bytes(instruction, 'utf-8'))
+    print('Client: sent instruction to server: ' + instruction)
+
 
     data_string = str(randomBall)+' & '+ str(distance)
     server.send(bytes(data_string,'utf-8'))
@@ -73,6 +229,27 @@ def sendToRobot(randomBall,distance):
     server.close()
 
     return buffer
+
+
+def moveForward(distance):
+    instruction = 'moveforward' +str(distance)
+    response = sendToRobot(instruction)
+    return response
+
+def moveBackward(distance):
+    instruction = 'movebackward'+str(distance)
+    response = sendToRobot(instruction)
+    return response
+
+def turnLeft():
+    instruction = 'turnleft'
+    response = sendToRobot(instruction)
+    return response
+
+def turnRight():
+    instruction = 'turnright'
+    response = sendToRobot(instruction)
+    return response
     
 
 if __name__ == '__main__':
